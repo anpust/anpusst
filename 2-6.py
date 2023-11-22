f490617d69a03cffbdcbf0bc903909d2a6e64258
anpusst <150606300+anpust@users.noreply.github.com>
2023-11-22 10:53:45 +0300

    Update 2-6.py

--- 2-6.py
+++ 2-6.py
@@ -1,1 +1,10 @@
+import re
 
+def remove_text_in_brackets(text):
+    pattern = r"\([^()]*\)"
+    result = re.sub(pattern, "", text)
+    return result
+
+text = "Пример (текста) с (скобками)."
+processed_text = remove_text_in_brackets(text)
+print(processed_text)
