e6eae7b121345c44fc3a3a8c3a183ce5cb9bd60c
anpusst <anpusst@gmail.com>
2023-12-13 11:39:55 +0300

    0

--- 2-6.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def remove_text_in_brackets(text):
-    pattern = r"\([^)]*\)"
-    return re.sub(pattern, "", text)
-
-text = "Это (текст), в котором нужно удалить (часть текста, заключенную) в скобки."
-cleaned_text = remove_text_in_brackets(text)
-print(cleaned_text)
